dcf669b3ca8f658f61679e601938f01c22e225a6
Matteo Nastasi <nastasi@openquake.org>
2025-01-10 18:19:48 +0100

    command refactoring with more parameters to both validate commands and refactoring parameters management using argparse package

--- openquake/gem_taxonomy/scripts.py
+++ openquake/gem_taxonomy/scripts.py
@@ -17,7 +17,10 @@
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 import sys
 import csv
-from openquake.gem_taxonomy import GemTaxonomy
+import json
+import argparse
+from argparse import RawTextHelpFormatter
+from openquake.gem_taxonomy import GemTaxonomy, __version__
 from parsimonious.exceptions import ParseError as ParsimParseError
 from parsimonious.exceptions import (IncompleteParseError as
                                      ParsimIncompleteParseError)
@@ -28,88 +31,118 @@ def info():
 
 
 def validate():
+    parser = argparse.ArgumentParser(
+        description='Validate taxonomy string (version 3.3).')
+    parser.add_argument(
+        'taxonomy_str', type=str, help='The taxonomy string to validate')
+    parser.add_argument(
+        '-c', '--canonical', action='store_true',
+        help='return 0 if taxonomy_str is a canonical taxonomy string only')
+    parser.add_argument(
+        '-r', '--report', action='store_true',
+        help=('dump a json with information about canonicity of taxonomy_str:'
+              ' {"is_canonical": true} if canonical, else {"is_canonical":'
+              ' false, "canonical": "<canonical_taxonomy_str>"}'))
+    parser.add_argument('-V', '--version', action='version',
+                        version='%s' % __version__,
+                        help='show application version and exit')
+
+    args = parser.parse_args()
+
     gt = GemTaxonomy()
 
     try:
-        gt.validate(sys.argv[1])
+        report = gt.validate(args.taxonomy_str)
+        if args.report:
+            print(json.dumps(report))
     except (ValueError, ParsimParseError,
             ParsimIncompleteParseError) as exc:
         print(str(exc))
         sys.exit(1)
-    sys.exit(0)
-
-
-def validate_csv_usage(ret):
-    print("""
-SYNOPSIS
-    %s CSVFILE [-n] FIELD1 [FIELD2 [...]]
-
-DESCRIPTION
-    Validates field[s] of csv file as valid taxonomy string.
-    Fields are identified with lowercase names or indexes  if '-n'
-    optional parameter is specified.
-
-    Exit status:
-    0          if all taxonomies are valid
-    1          at least one taxonomy string is invalid
-
-    Note:
-    If some taxonomy string is valid but not in the canonical form
-    a line will be printed in the form:
-        original_string '|' canonical_form
-
-""")
-    sys.exit(ret)
+    if args.canonical:
+        sys.exit(0 if report['is_canonical'] else 1)
+    else:
+        sys.exit(0)
 
 
 def csv_validate():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Validates field[s] of csv file as valid GEM taxonomy string."
+            "Fields are identified with lowercase names or indexes  if '-n'"
+            " optional parameter is specified"),
+        epilog=(
+            '''exit status:
+    0          if all taxonomies are valid
+    1          at least one taxonomy string is invalid
+
+note:
+    If some taxonomy string is valid but not in the canonical form a line will be printed in the form:
+    "filename|row_num|column|original_taxonomy|0|canonical_taxonomy"
+
+    If some taxonomy result not valid a line will be printed in the form:
+    "filename|row_num|column|original_taxonomy|1|error_message"'''),
+        formatter_class=RawTextHelpFormatter
+    )
+    parser.add_argument(
+        '-c', '--canonical', action='store_true',
+        help='return 0 if taxonomy strings are all canonical GEM taxonomy'
+        ' string only')
+    parser.add_argument(
+        '-n', '--numerical', action='store_true',
+        help='fields are indicate by their numerical index')
+    parser.add_argument(
+        'csvfile', type=str, help='The CSV file with taxonomy string columns'
+        ' to validate')
+    parser.add_argument(
+        'field', nargs='+',
+        help=("Fields are identified with lowercase names or indexes"
+              " if '-n' optional parameter is specified."))
+    parser.add_argument('-V', '--version', action='version',
+                        version='%s' % __version__,
+                        help='show application version and exit')
+
+    args = parser.parse_args()
+
     gt = GemTaxonomy()
-
-    if len(sys.argv) < 2 or (
-            sys.argv[1] == '-n' and len(sys.argv) < 3):
-        validate_csv_usage(1)
-
-    if sys.argv[1] == '-n':
-        use_indexes = True
-        csv_fields = sys.argv[2:]
-    else:
-        use_indexes = False
-        csv_fields = sys.argv[1:]
 
     taxonomy_cols = []
     name_cols = {}
-    csvfilename = sys.argv[1]
-    with open(csvfilename) as csvfile:
+    with open(args.csvfile) as csvfile:
         csvreader = csv.reader(csvfile)
-        if not use_indexes:
+        if args.numerical:
+            taxonomy_cols = [int(idx) for idx in args.field]
+        else:
             header = next(csvreader)
             for col, fieldname in enumerate(header):
-                if fieldname.lower() in csv_fields:
+                if fieldname.lower() in args.field:
                     taxonomy_cols.append(col)
                     name_cols[col] = fieldname
             if len(taxonomy_cols) == 0:
                 print("No header fieldnames match fields passed as arguments",
                       file=sys.stderr)
-        else:
-            taxonomy_cols = [int(idx) for idx in csv_fields]
 
         ret_code = 0
         for idx, row in enumerate(csvreader):
             for col in taxonomy_cols:
                 tax = row[col]
                 try:
-                    gt.validate(tax)
+                    report = gt.validate(tax)
+                    if report['is_canonical'] is False:
+                        print('%s|%d|%s|%s|%d|%s' % (
+                            args.csvfile, idx, col, tax, 0,
+                            report['canonical']))
+                        if args.canonical is True:
+                            ret_code = 1
                 except (ValueError, ParsimParseError,
                         ParsimIncompleteParseError) as exc:
                     ret_code = 1
-                    if use_indexes:
-                        # import pdb ; pdb.set_trace()
-                        print('%s|%s|%d|%s|%s' % (
-                            csvfilename, col, idx, tax, str(exc)),
-                              file=sys.stderr)
+                    if args.numerical:
+                        print('%s|%d|%s|%s|%d|%s' % (
+                            args.csvfile, idx, col, tax, 1,
+                            str(exc)))
                     else:
-                        # import pdb ; pdb.set_trace()
-                        print('%s|%s|%d|%s|%s' % (
-                            csvfilename, name_cols[col], idx, tax,
-                            str(exc)), file=sys.stderr)
+                        print('%s|%d|%s|%s|%d|%s' % (
+                            args.csvfile, idx, name_cols[col], tax, 1,
+                            str(exc)))
         sys.exit(ret_code)
